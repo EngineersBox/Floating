39d333948e79d26d6388d5c0252cee9b75c37c01
EngineersBox <toastcraft.info@gmail.com>
2020-02-16 14:57:01 +1100

    Added nav icons and tool tip with animation

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ function showCredits() {
         document.getElementById("creditations").style.visibility = "hidden";
     }
 }
+
+function showTooltip(id, target) {
+    const t = document.getElementById(target).style;
+    document.getElementById(id).style.marginLeft = '0.5em';
+    t.opacity = 0.9;
+    t.transform = 'translateX(0em)';
+}
+
+function hideTooltip(id, target) {
+    const t = document.getElementById(target).style;
+    document.getElementById(id).style.marginLeft = 0;
+    t.opacity = 0;
+    t.transform = 'translateX(2em)';
+}
