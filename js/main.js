918b4e6399444170a1e10a27d0ed1cdea9db67d3
EngineersBox <toastcraft.info@gmail.com>
2020-02-16 19:40:05 +1100

    Added responsive navbar positioning and sizing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+document.querySelector('input[name=mode]').addEventListener('change', function() {
+    for (const elem of Array.from(document.all)) {
+        elem.classList.add('transition');
+    }
+    window.setTimeout(() => {
+        for (const elem of Array.from(document.all)) {
+            elem.classList.remove('transition');
+        }
+    }, 250);
+    document.documentElement.setAttribute('data-theme', this.checked ? 'dark' : 'light');
+});
+
+const scrollChanged = [
+    "sidebar",
+    "tabs",
+    "tooltip-container",
+    "tooltip",
+    "tab"
+];
+
+window.addEventListener('scroll', function() {
+    const scrollPoint = window.scrollY > 200;
+    for (const name of scrollChanged) {
+        for (const elem of Array.from(document.getElementsByClassName(name + (scrollPoint ? "" : "-top")))) {
+            elem.classList.remove(name + (scrollPoint ? "" : "-top"));
+            elem.classList.add(name + (!scrollPoint ? "" : "-top"));
+        }
+    }
+});
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 830) {
+        return;
+    }
+    if (window.scrollY <= 200) {
+        // SHOW NORMAL TABS
+        return;
+    }
+    //SHOW BURGER MENU
+});
+
 function showCredits() {
     let showCredits = document.getElementById("show-credits").checked;
     if (showCredits) {
@@ -11,14 +52,24 @@ function showCredits() {
 
 function showTooltip(id, target) {
     const t = document.getElementById(target).style;
-    document.getElementById(id).style.marginLeft = '0.5em';
+    const d = document.getElementById(id).style;
+    if (window.scrollY > 200) {
+        d.marginTop = "0.5em";
+    } else {
+        d.marginLeft = "0.5em";
+    }
     t.opacity = 0.9;
-    t.transform = 'translateX(0em)';
+    t.transform = 'translate' + (window.scrollY > 100 ? 'Y' : 'X') + '(0em)';
 }
 
 function hideTooltip(id, target) {
     const t = document.getElementById(target).style;
-    document.getElementById(id).style.marginLeft = 0;
+    const d = document.getElementById(id).style;
+    if (window.scrollY > 200) {
+        d.marginTop = 0;
+    } else {
+        d.marginLeft = 0;
+    }
     t.opacity = 0;
-    t.transform = 'translateX(2em)';
+    t.transform = 'translate' + (window.scrollY > 100 ? 'Y' : 'X') + '(2em)';
 }
